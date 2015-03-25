6ce52fd50d85c9167ba766777fd4e5919589f663
Jason Beatty <jasondavidbeatty@gmail.com>
2015-03-25 16:14:16 -0400

    Display flash messages upon saving (or failing to save) posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,9 +10,13 @@ class PostsController < ApplicationController
   def create
     post = Post.new(params.require(:post).permit(:title, :link, :body))
     if post.save
-      redirect_to posts_path
+      redirect_to posts_path, flash: { notice: "Your post was saved successfully" }
     else
-      redirect_to new_post_path
+      # redirect_to new_post_path, flash: { error: post.errors.full_messages }
+      # flashes error to current page
+      flash.now[:error] = post.errors.full_messages
+      @post = post
+      render :new
     end
   end
 
