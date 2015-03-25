4fc9ba2e657df4c7723e9b093598b9570dce7f5b
Jason Beatty <jasondavidbeatty@gmail.com>
2015-03-25 18:52:27 -0400

    Add conditional validations for links and text posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ActiveRecord::Base
   validates :title, length: { maximum: 255 }, presence: true
+  validates :body, presence: true, if: :text?
+  validates :link, presence: true, if: :link?
   enum post_type: [:link, :text]
 end
