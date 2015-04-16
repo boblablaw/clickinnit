9eb64a42605bca5d7e2658635b9b215e1096701c
Jason Beatty <jasondavidbeatty@gmail.com>
2015-04-15 23:46:27 -0400

    change name to clickinnit

--- db/schema.rb
+++ db/schema.rb
@@ -85,7 +85,6 @@ ActiveRecord::Schema.define(version: 20150404175240) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "elevennote_api_key"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
